6e893ed1db30b1dd4eabc26d09dd3860a5dfb0ef
KsuMor <ksumor01@gmail.com>
2022-07-21 17:59:28 +0500

    Searching for date. Creating empty sql table. Saving the results.

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ from typing import Optional
 
 class Cheaters:
 
-    def __init__(self, client_path, server_path):
+    def __init__(self):
         self.client = pd.read_csv(client_path)
         self.server = pd.read_csv(server_path)
         self.df = Optional[pd.DataFrame]
-        self.db_table = Optional[pd.DataFrame]
+        self.db_table = self.read_sql()
 
-    def edit_data(self, date):
+    def edit_data(self):
+        # the goal date
+
+        def df_on_date(date, df):
+            return df[df.timestamp.map(lambda x: datetime.datetime.fromtimestamp(x).date())
+                      == datetime.datetime.fromisoformat(date).date()]
+
+        self.client = df_on_date(date_str, self.client)
+        self.server = df_on_date(date_str, self.server)
 
         # merge 2 tables
         self.df = self.client.merge(self.server, on='error_id', how='outer')
@@ -49,8 +57,6 @@ class Cheaters:
                                           'description_y': 'json_server'})
         print(self.df.columns)
 
-
-
     def read_sql(self):
         conn = sqlite3.connect(database_path)
         sql_query = pd.read_sql_query(f'''
@@ -59,11 +65,32 @@ class Cheaters:
                                        FROM {table_name}
                                        ''', conn)
 
-        self.db_table = pd.DataFrame(sql_query)
+        db = pd.DataFrame(sql_query)
+
+        conn.close()
+        return db
+
+    def create_empty_sqlite_table(self):
+
+        conn = sqlite3.connect(f'{output_table_name}.db')
+        cur = conn.cursor()
+        cur.execute(f"""CREATE TABLE IF NOT EXISTS {output_table_name}(
+                           {col_names[0]} INT PRIMARY KEY,
+                           {col_names[1]} TEXT,
+                           {col_names[2]} TEXT,
+                           {col_names[3]} TEXT,
+                           {col_names[4]} TEXT,
+                           {col_names[5]} TEXT);
+                        """)
+        conn.commit()
         conn.close()
 
     def csv_to_sqlite(self):
-        pass
+        con = sqlite3.connect(f'{output_table_name}.db')
+        self.df.to_sql(f'{output_table_name}', con, if_exists='replace', index=False)
+        # print(list(con.execute(f"select * from {output_table_name}")))
+        con.commit()
+        con.close()
 
 
 client_path = 'data/client.csv'
@@ -71,9 +98,13 @@ server_path = 'data/server.csv'
 database_path = 'data/cheaters.db'
 table_name = 'cheaters'
 date_str = '2021-03-07'
-date_time = datetime.datetime.fromisoformat(date_str)
+output_table_name = 'output3'
 
-ch = Cheaters(client_path, server_path)
+col_names = ['timestamp', 'player_id', 'event_id', 'error_id', 'json_server', 'json_client']
+
+ch = Cheaters()
+ch.create_empty_sqlite_table()
 ch.read_sql()
-ch.edit_data(date_time)
+ch.edit_data()
+ch.csv_to_sqlite()
 
